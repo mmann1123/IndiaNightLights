3585ef3242a7884bdc507170c3dfb6211d7fd6a7
mmann1123 <mmann1123@gmail.com>
2015-08-07 16:13:00 -0400

    Server files backup

---
+++ job_script_May2015.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set output and error output filenames, %j will be replaced by Slurm with the jobid
+#SBATCH -o testing%j.out
+#SBATCH -e testing%j.err 
+ 
+# single node in the "short" partition
+#SBATCH -N 1
+#SBATCH -p 128gb
+
+# half hour timelimit
+#SBATCH -t 48:00:00
+
+
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=mmann1123@gwu.edu  
+
+module load proj.4/4.8.0
+module load gdal
+module load R/3.0.2
+module load gcc/4.9.0
+
+# Run R code 
+srun R CMD BATCH ./grid_viirs_data_server_May2015.R
