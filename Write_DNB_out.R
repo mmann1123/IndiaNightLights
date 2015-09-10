9fce4cf1dd826dee157c6b10fe6cb9a0e82ae637
unknown <mmann@CCAGEO202HMMANN.cloud.ccas.gwu.edu>
2015-09-10 11:04:43 -0400

    commit changes to match server version

--- Write_DNB_out.R
+++ Write_DNB_out.R
@@ -3,10 +3,19 @@
 # it takes RData files created on the server and writes out tif files
 # Limit extent to Maharashstra
 
+# Run the following in bash before starting R
+#module load proj.4/4.8.0
+#module load gdal
+#module load R/3.0.2
+#module load gcc/4.9.0
+# R
+
+
+
 library(raster)
+library(rgdal)
 
-
-setwd("C:\\Users\\mmann\\Desktop\\NightTimeData\\May2015/")
+setwd("/groups/manngroup/India VIIRS/2015/hdf/")
 
 # mararashtra extent westlimit=71.99; southlimit=15.03; eastlimit=81.51; northlimit=22.5
 # nice ideas on extent and resolution http://stackoverflow.com/questions/20733555/how-to-create-a-raster-brick-with-rasters-of-different-extents
@@ -21,30 +30,9 @@ test_intersection <- function(a,b){
 }
 
 
-# Write out cloud band data -------------------------------------------
-
-output_list=list.files(getwd(),pattern='output2')
-
-for(i in 1:length(output_list)){
-    load(paste(getwd(),'/',output_list[i],sep=''))
-    
-    if(length(output2)==0 ){ # check forempty lists
-        warning(paste('error in ',output_list[i])); next}
-   
-     lapply(1:length(output2),function(x) if(class(output2[[x]])=='RasterLayer' & test_intersection(output2[[x]],s) ){  # avoid empty and non overlapping images
-        print(x)
-        output2[[x]]=resample(output2[[x]], s, method="ngb") # limit to new extent and res
-        if(x==1){windows(); image(output2[[x]])}
-        writeRaster(output2[[x]], filename=paste(getwd(),'/',
-                output2[[x]]@data@names,'_cld_v3.tif',sep=""),format='GTiff',overwrite=TRUE)})
-}    
-
-# download job_output2_5_v3.RData  and correct output there was an error
-
-
 # Write out day night band data -------------------------------------------
 
-output_list=list.files(getwd(),pattern='output1')
+output_list=list.files(getwd(),pattern='job_dnb_')
 
 for(i in 1:length(output_list)){
     load(paste(getwd(),'/',output_list[i],sep=''))
@@ -53,13 +41,75 @@ for(i in 1:length(output_list)){
         next}
     lapply(1:length(output),function(x) if(class(output[[x]])=='RasterLayer'& test_intersection(output[[x]],s) ){ 
         print(x)
-        if(x==1){ windows(); image(output[[x]])}
         
         output[[x]]=resample(output[[x]], s, method="bilinear")
         
         writeRaster(output[[x]], filename=paste(getwd(),'/',
-            output[[x]]@data@names,'_dnb_v3.tif',sep=""),format='GTiff',overwrite=TRUE)})
+                                                substr( output[[x]]@data@names,2,13),'_dnb_v3.tif',sep=""),format='GTiff',overwrite=TRUE)})
 }    
+remove(output1)
+
+
+# Write out cloud band data -------------------------------------------
+
+output_list=list.files(getwd(),pattern='job_cld_')
+
+for(i in 1:length(output_list)){
+    load(paste(getwd(),'/',output_list[i],sep=''))
+    
+    if(length(output2)==0 ){ # check forempty lists
+        warning(paste('error in ',output_list[i])); next}
+    
+    lapply(1:length(output2),function(x) if(class(output2[[x]])=='RasterLayer' & test_intersection(output2[[x]],s) ){  # avoid empty and non overlapping images
+        print(x)
+        output2[[x]]=resample(output2[[x]], s, method="ngb") # limit to new extent and res
+        
+        writeRaster(output2[[x]], filename=paste(getwd(),'/',
+                                                 substr( output2[[x]]@data@names,2,13),'_cld_v3.tif',sep=""),format='GTiff',overwrite=TRUE)})
+}
+remove(output2)
+
+
+
+# Write out moon zenith angle band data -------------------------------------------
+
+output_list=list.files(getwd(),pattern='job_zen_')
+
+for(i in 1:length(output_list)){
+    load(paste(getwd(),'/',output_list[i],sep=''))
+    
+    if(length(output3)==0 ){ # check forempty lists
+        warning(paste('error in ',output_list[i])); next}
+    
+    lapply(1:length(output3),function(x) if(class(output3[[x]])=='RasterLayer' & test_intersection(output3[[x]],s) ){  # avoid empty and non overlapping images
+        print(x)
+        output3[[x]]=resample(output3[[x]], s, method="ngb") # limit to new extent and res
+        
+        writeRaster(output3[[x]], filename=paste(getwd(),'/',
+                                                 output3[[x]]@data@names,'_zen_v3.tif',sep=""),format='GTiff',overwrite=TRUE)})
+}
+remove(output3)
+
+
+
+# Write out moon zenith angle band data -------------------------------------------
+
+output_list=list.files(getwd(),pattern='job_azt_')
+
+for(i in 1:length(output_list)){
+    load(paste(getwd(),'/',output_list[i],sep=''))
+    
+    if(length(output4)==0 ){ # check forempty lists
+        warning(paste('error in ',output_list[i])); next}
+    
+    lapply(1:length(output4),function(x) if(class(output4[[x]])=='RasterLayer' & test_intersection(output4[[x]],s) ){  # avoid empty and non overlapping images
+        print(x)
+        output4[[x]]=resample(output4[[x]], s, method="ngb") # limit to new extent and res
+        
+        writeRaster(output4[[x]], filename=paste(getwd(),'/',
+                                                 output4[[x]]@data@names,'_azt_v3.tif',sep=""),format='GTiff',overwrite=TRUE)})
+}
+remove(output4)
 
 
 # # code to change names etc
@@ -68,5 +118,4 @@ for(i in 1:length(output_list)){
 # for(i in 1:length(output_list)){
 #     file.rename(output_list[i],substr(output_list[i],2,30) )  #paste('2',substr(output_list[i],3,24),sep='')
 # }
-
 
