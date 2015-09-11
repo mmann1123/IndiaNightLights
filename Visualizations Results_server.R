d63bca98bf0b050ba2441a2647de5597d429842b
mmann1123 <mmann1123@login3.colonialone.gwu.edu>
2015-09-11 15:50:44 -0400

    adding random point selection

--- Visualizations Results_server.R
+++ Visualizations Results_server.R
@@ -343,7 +343,41 @@ ggplot(combined3, aes(count, dnb,colour=pred_actual))+geom_point()
 
 
 
+# Demeaned regression on 1000 point sample -------------------------------------------
+PolygonFromExtent <-
+function(ext, asSpatial=T, crs=CRS(NA), id=1)
+{
+  if(class(ext)== "rasterLayer")
+  {
+    # if raster supplied determine extent and crs then proceed
+    crs <- ext@crs
+    ext <- extent(ext)
+  }
+  if(class(ext)== "Extent")
+	x1 <- ext@xmin
+	x2 <- ext@xmax
+	y1 <- ext@ymin
+	y2<-ext@ymax
+	
+	coords <- matrix(c(x1, y1, 
+					   x1, y2,
+					   x2, y2,
+					   x2, y1,
+					   x1, y1), ncol=2, byrow=T)
+					 
+	poly <- Polygon(coords)
+	if(asSpatial)
+	{
+		spPoly <- SpatialPolygons(list(Polygons(list(poly), ID=id)), proj4string=crs)
+		return(spPoly)
 
+	}
+	return(poly)
+	
+}
+
+set.seed(2)
+sampled = spsample(PolygonFromExtent(extent(72, 81.50, 15, 22.5),crs=CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ')),n=1000,type='random')
 
 
 
