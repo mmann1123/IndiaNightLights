999255e7efb5a98dded9d8f4141b80b9b6bd9fb9
mmann1123 <mmann1123@login3.colonialone.gwu.edu>
2015-09-15 16:26:06 -0400

    adding kmean clustering pre regression

--- Visualizations Results_server.R
+++ Visualizations Results_server.R
@@ -333,17 +333,17 @@ ggplot(combined[combined$pred_actual == 'resid+const',], aes(count, dnb,colour=p
   facet_wrap(~ location, scales="free_y")
 
 
-# test if residual is white noise
+# test if residual is white noise  NOT NEEDED TOO STRICT
 #H0: The data are independently distributed (i.e. the correlations in the population from which the sample is taken are 0, so that any observed correl$
 #Ha: The data are not independently distributed; they exhibit serial correlation.
-
-library(zoo)
-for (location in as.character(unique(combined$location))){
-   test_data = combined[combined$location==location,]
-   test_data_ag = aggregate(dnb~date.time,data=test_data, FUN=mean)
-   test_data_ts = zoo(test_data_ag$dnb,test_data_ag$date.time)
-   print( Box.test(test_data_ts, lag = 1, type = c("Ljung-Box"), fitdf = 0))
-  }
+#
+#library(zoo)
+#for (location in as.character(unique(combined$location))){
+#   test_data = combined[combined$location==location,]
+#   test_data_ag = aggregate(dnb~date.time,data=test_data, FUN=mean)
+#   test_data_ts = zoo(test_data_ag$dnb,test_data_ag$date.time)
+#   print( Box.test(test_data_ts, lag = 1, type = c("Ljung-Box"), fitdf = 0))
+#  }
 
 
 
@@ -379,6 +379,16 @@ for (location in as.character(unique(combined$location))){
 
 # Demeaned regression on 1000 point sample -------------------------------------------
 
+
+# load data
+setwd('/groups/manngroup/India\ VIIRS/2015')
+
+load('dnb_stack_wo_cld.RData')    # start here cloud free images stored here.
+load('zen_stack_wo_cld.RData')
+load('azt_stack_wo_cld.RData')
+
+
+# create a plygon to create samples within
 PolygonFromExtent <- function(ext, asSpatial=T, crs=CRS(NA), id=1)
 {
   if(class(ext)== "rasterLayer")
@@ -399,8 +409,10 @@ PolygonFromExtent <- function(ext, asSpatial=T, crs=CRS(NA), id=1)
 	return(poly)
 }
 
+# create sample of 10000 set seed for reproducibility
 set.seed(2)
-sampled = spsample(PolygonFromExtent(extent(72, 81.50, 15, 22.5),crs=CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ')),n=10000,type='random')
+sampled = spsample(PolygonFromExtent(extent(72, 81.50, 15, 22.5),crs=CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ')),
+     n=10000,type='random')
 
 # extract data (and surrounding area)
 dnb_values = extract(dnb_stack,sampled,fun= function(x) mean(x,na.rm=T), df=T)#buffer=1.2e3,
@@ -433,37 +445,70 @@ phase$date.time = paste(phase$year,sprintf('%03d',(phase$doy)),'.',phase$time,se
 
 
 # add moon phase
+
 library(plyr)
 dnb_values = join(dnb_values,zen_values)
 dnb_values = join(dnb_values,azt_values)
 dnb_values = join(dnb_values, phase)
 
 
-# compare actual and resid plus constant for demeaned regression
+# find k-means = 3 groups based on dnb values (based on mean and sd of values)
+library(flexclust)   # allow for multidimentional clustering with predict function
+library(sampling)
+na_dnb = na.omit(dnb_values)
+# cluster based on mean and sd of data 
+sd_dnb = aggregate(dnb~location,data=na_dnb,function(x) sd(x,na.rm=T))
+mn_dnb = aggregate(dnb~location,data=na_dnb,function(x) mean(x,na.rm=T))
+names(sd_dnb)=c('location','sd_dnb')
+names(mn_dnb)=c('location','mn_dnb')
+na_dnb = join(mn_dnb, sd_dnb,by='location')  # store mean and sd of dnb values for each location
+# stratified sample to make sure we have adequate representation for each land use class
+na_dnb$quantile = cut(na_dnb$mn_dnb,quantile(na_dnb[,'mn_dnb']))
+set.seed(2)
+s=strata(na_dnb,'quantile',size=c(250,250,250,250,1), method="srswor") # create strata based on quantiles
+na_dnb_sample = getdata( na_dnb,s)
+cl1 = kcca(na_dnb_sample[,c('mn_dnb','sd_dnb')], k=5, kccaFamily("kmeans"))
+pred_test = predict(cl1, newdata=na_dnb[,c('mn_dnb','sd_dnb'),drop=F])
+
+
+# OLD KMEANS METHOD
+#dnb_num = data.frame(dnb=dnb_values$dnb)   # set up a dataframe to store row numbers so can be joined later
+#row.names(dnb_num)
+#kmn= kmeans(na.omit(dnb_num),3)
+#kmn = data.frame(row = names(kmn$cluster),kmn = kmn$cluster)
+#dnb_values$row = row.names(dnb_values)
+#dnb_values = join(dnb_values, kmn)
+
+
+# compare actual and resid plus constant for demeaned regression using kmean cluster for slope & intercept dummies
 
 mean_dnb = aggregate(dnb~location,data=dnb_values,function(x) mean(x,na.rm=T))
 names(mean_dnb)=c('location','mean_dnb')
 dnb_values = join(dnb_values, mean_dnb,by='location')
 dnb_values$demean_dnb = dnb_values$dnb - dnb_values$mean_dnb
 
-mean_lm = lm(demean_dnb~0+I(mean_dnb>1.5e-8)*(ns(zen,df=3)+ns(azt,df=3)+ns(phase,df=3)+
-      zen:azt+poly(zen:phase,2)+azt:phase),data=dnb_values) # omit intercept
+mean_lm = lm(demean_dnb~0+factor(kmn)*(ns(zen,df=3)+ns(azt,df=3)+ns(phase,df=3)+zen*azt+
+      ns(zen*phase,3)+ns(azt*phase,3)),data=dnb_values) # omit intercept
 
-mean_lm = lm(demean_dnb~0+I(mean_dnb<0.5e-8)*(ns(zen,df=3)+ns(azt,df=3)+ns(phase,df=3)+
-      zen*azt+ns(zen*phase,3)+ns(azt*phase,3))
-       +I(mean_dnb>1.5e-8)*(ns(zen,df=3)+ns(azt,df=3)+ns(phase,df=3)+
-      zen*azt+ns(zen*phase,3)+ns(azt*phase,3)),data=dnb_values) 
+mean_lm = lm(demean_dnb~0+factor(kmn)*(ns(zen*azt,2)+
+      ns(zen*phase,2)+ns(azt*phase,2)),data=dnb_values) # omit intercept
+
 
 summary(mean_lm)
 
-# look at distribution of values
-qplot(mean_dnb, data=mean_dnb, geom="histogram",binwidth=diff(range(mean_dnb$mean_dnb))/100)
-qplot(mean_dnb, data=mean_dnb, geom="histogram",binwidth=diff(range(mean_dnb$mean_dnb))/100)+coord_cartesian(xlim = c(0,5e-8))
 
-table(cut(mean_dnb$mean_dnb, breaks=c(0, 1e-9,,1e-8,1.5e-8,2e-8,10), include.lowest=TRUE))
-quantile(mean_dnb$mean_dnb)
+# compare actual and predicted
 
+actual = na.omit(dnb_values)
+actual$count = 1:dim(actual)[1]
+actual$pred_actual = 'actual'
 
+pred = actual
+pred$pred_actual = 'predicted'
+pred$count = 1:dim(pred)[1]
+pred = join(pred,mean_dnb,by='location')
+pred$dnb = mean_lm$fitted.values+pred$mean_dnb   # add back the mean from demeaning process
+head(pred)
 
 resid3 = actual
 resid3$pred_actual = 'resid+const'
@@ -472,27 +517,30 @@ resid3 = join(resid3,mean_dnb,by='location')
 resid3$dnb = mean_lm$residuals  +resid3$mean_dnb
 head(resid3)
 
-pred3 = actual
-pred3$pred_actual = 'predicted'
-pred3$count = 1:dim(pred3)[1]
-pred3 = join(pred3,mean_dnb,by='location')
-pred3$dnb = mean_lm$fitted.values+pred3$mean_dnb
-head(pred3)
+combined = rbind.fill(actual,pred, resid3)
 
 
-combined = rbind.fill(actual,resid3,pred3)
+# sample a portion for graphing
+
+library(sampling)
+set.seed(2)
+s=strata(combined,c("kmn"),size=c(5,5,5), method="srswor")
+combined_sample = getdata(combined,s)  # only get one observation for each location 
+combined_sample = combined[combined$location %in% combined_sample$location,]   # limit to desired locations keeping all observations
+
 
 # plot actual vs predicted
-ggplot(combined, aes(count, dnb,colour=pred_actual))+geom_point()+
+
+ggplot(combined_sample, aes(count, dnb,colour=pred_actual))+geom_point()+
   facet_wrap(~ location, scales="free_y")
 
-ggplot(combined[combined$pred_actual != 'resid+const',], aes(count, dnb,colour=pred_actual))+geom_point()+
+ggplot(combined_sample[combined_sample$pred_actual != 'resid+const',], aes(count, dnb,colour=pred_actual,alpha=0.2))+geom_point()+
   facet_wrap(~ location, scales="free_y")
 
-ggplot(combined[combined$pred_actual != 'resid+const',], aes(count, dnb,colour=pred_actual))+geom_point()+
+ggplot(combined_sample[combined_sample$pred_actual != 'resid+const',], aes(count, dnb,colour=pred_actual))+geom_point()+
   facet_wrap(~ location)
 
-ggplot(combined[combined$pred_actual == 'resid+const',], aes(count, dnb,colour=pred_actual))+geom_point()+
+ggplot(combined_sample[combined_sample$pred_actual == 'resid+const',], aes(count, dnb,colour=pred_actual))+geom_point()+
   facet_wrap(~ location, scales="free_y")
 
 
@@ -611,3 +659,13 @@ endCluster()
 #   
 #   dnb_buffer_values =  extract(dnb_stack,locations_buffer)
 #   
+
+
+
+# look at distribution of values
+#qplot(mean_dnb, data=mean_dnb, geom="histogram",binwidth=diff(range(mean_dnb$mean_dnb))/100)
+#qplot(mean_dnb, data=mean_dnb, geom="histogram",binwidth=diff(range(mean_dnb$mean_dnb))/100)+coord_cartesian(xlim = c(0,5e-8))
+#
+#table(cut(mean_dnb$mean_dnb, breaks=c(0, 1e-9,,1e-8,1.5e-8,2e-8,10), include.lowest=TRUE))
+#quantile(mean_dnb$mean_dnb)
+
